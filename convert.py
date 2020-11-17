d4ffd652f2cb90a1fdcc2db12c9d485c8ed8f550
Patrick Demers <patrickdemers6@gmail.com>
2020-11-16 22:47:20 -0600

    update var names in convert_coordinates

--- convert.py
+++ convert.py
@@ -4,17 +4,22 @@ from labels import get_labels
 
 
 def convert_coordinates(size, box):
-    dw = 1.0 / size[0]
-    dh = 1.0 / size[1]
+    width_percent_per_pixel = 1.0 / size[0]
+    height_percent_per_pixel = 1.0 / size[1]
+
     x = (box[0] + box[1]) / 2.0
     y = (box[2] + box[3]) / 2.0
-    w = box[1] - box[0]
-    h = box[3] - box[2]
-    x = x * dw
-    w = w * dw
-    y = y * dh
-    h = h * dh
-    return (x, y, w, h)
+
+    width = box[1] - box[0]
+    height = box[3] - box[2]
+
+    x = x * width_percent_per_pixel
+    width = width * width_percent_per_pixel
+
+    y = y * height_percent_per_pixel
+    height = height * height_percent_per_pixel
+
+    return x, y, width, height
 
 
 def convert(path):
